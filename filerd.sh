aa4667efd9f841a403009f14886846bec6199f61
satyakaibm <satyabratakaran09@gmail.com>
2023-07-27 14:32:09 +0200

    add filerd script and sample passwd into feature1.0 branch

---
+++ filerd.sh
@@ -0,0 +1,10 @@
+echo "Reading each line and printing only the Users one by one"
+filename='sample_passwd'
+
+count=1
+while read -r line; do
+#echo "$count: $line"
+usr1=$(echo $line | awk -F: '{print $1}')
+echo "$count: $usr1"
+count=$((count+1))
+done < $filename
